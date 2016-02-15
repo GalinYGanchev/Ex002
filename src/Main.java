d2db08b2cb2cb6d1288559deb647b5bc3ccf3315
group <galin_psy@abv.bg>
2016-02-15 21:17:48 +0200

    secont attempt

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int num = 51;
+		System.out.println(num);
+
+		int nm1 = num;
+		System.out.println(nm1);
+
+		num = 102;
+		System.out.println(num);
+		System.out.println(nm1);
+
+		// int d;
+		// System.out.println(d);
+
+	}
+
+}
